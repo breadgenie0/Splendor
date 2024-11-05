1910ce612981e495fa7c39a0851866f3c25786e3
breadgenie0 <karenwangusa@outlook.com>
2024-11-05 09:07:54 -0600

    Update Token.java

--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,16 @@
 import java.util.*;
+import java.awt.image.BufferedImage;
 
 public class Token {
-    
+	private String suite;
+	private BufferedImage image;
+	
+	public Token(String s, BufferedImage b) {
+		suite = s;
+		image = b;
+	}
+	public String getSuite() {
+		return suite;
+	}
+
 }
