5ad36680bca4c8210472a3b645d9db65b63a0b64
ffffddddgggg <sn0wdropxbl1ss@gmail.com>
2024-11-05 09:15:13 -0600

    ok fixed

--- src/Card.java
+++ src/Card.java
@@ -1,20 +1,32 @@
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
 
 
 public class Card {
 private int points;
-private Map<Token, Integer> price;
+private HashMap<Token, Integer> price;
 private String suite;
 private BufferedImage cardFront;
 private BufferedImage cardBack;
 private int level;
 
-public Card(String s, int l, Map<Token, Integer> p, BufferedImage cf, BufferedImaged cb) {
-
+public Card(int points, String s, int l, HashMap<Token, Integer> p, BufferedImage cf, BufferedImaged cb) {
+this.points = points;
+suite = s;
+level = l;
+price = p;
+cardFront = cf;
+cardBack = cb;
 }
 
-public 
+public HashMap getPrice(){
+    return price;
+}
 public int getPoints() {
 return points;
 }
--- src/Token.java
+++ src/Token.java
@@ -2,15 +2,8 @@ import java.util.*;
 import java.awt.image.BufferedImage;
 
 public class Token {
-	private String suite;
-	private BufferedImage image;
-	
-	public Token(String s, BufferedImage b) {
-		suite = s;
-		image = b;
-	}
-	public String getSuite() {
-		return suite;
-	}
-
+ private String suite;
+Private BufferedImage image;
+   
+   
 }
