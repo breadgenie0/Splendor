33aafdc4f137853901c63fc19634f665c2ef396e
ethanzhang175 <ethanzhang777@gmail.com>
2024-11-05 09:22:18 -0600

    hi

--- src/Patron.java
+++ src/Patron.java
@@ -1,5 +1,22 @@
 import java.util.*;
+import java.awt.image.BufferedImage;
 
 public class Patron {
-    
+    private ArrayList<String> cards;
+    private BufferedImage image;
+    public Patron (String c, BufferedImage i) {
+        int strLength = c.length();
+        for(int i = 0; i < strLength; i++) {
+            cards.add(c.substring(0,1));
+            c = c.subtring(1);
+        }
+        image = i;
+    }
+
+    public ArrayList<> getPrice() {
+        return cards;
+    }
+    public BufferedImage getImage() {
+        return image;
+    }
 }
