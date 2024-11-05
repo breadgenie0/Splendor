23e822537264f8dceee37fc1cdb2a8f3690f10f3
ffffddddgggg <sn0wdropxbl1ss@gmail.com>
2024-11-05 09:01:18 -0600

    yay cards

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class Card {
-//a b  d e f g apblskjdf hwhat the heck
+private int points;
+private Map<Token, Integer> price;
+private String suite;
+private BufferedImage cardFront;
+private BufferedImage cardBack;
+private int level;
+
+public Card(String s, int l, Map<Token, Integer> p, BufferedImage cf, BufferedImaged cb) {
+
+}
+
+public 
+public int getPoints() {
+return points;
+}
+
+public String getSuite() {
+    return suite;
+}
+
+public int getLevel(){
+    return level;
+}
+public BufferedImage getFront(){
+    return cardFront;
+}
+
+public BufferedImage getBack() {
+    return cardBack;
+}
 }
